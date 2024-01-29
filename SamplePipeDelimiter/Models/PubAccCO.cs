5edc7d68a23f7046936d9d5d7da71b9e80f4e347
Akila Peramatchi <dev.akila001@outlook.com>
2024-01-30 00:55:49 +0530

    Added models and DbContext

--- SamplePipeDelimiter/Models/PubAccCO.cs
+++ SamplePipeDelimiter/Models/PubAccCO.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SamplePipeDelimiter.Models
 {
-    internal class PubAccCO
+    [Table("PubAccCOs")]
+
+    public class PubAccCO
     {
+        [StringLength(2)]
+        [Column(name: "record_type", Order = 0)]
+        public string? RecordType { get; set; }
+
+        [Precision(10, 0)] //Note this is 9 in legacy.
+        [Column(name: "unique_system_identifier", Order = 1)]
+        public required decimal UniqueSystemIdentifier { get; set; }
+
+        [StringLength(14)]
+        [Column(name: "uls_file_num", Order = 2)]
+        public string? UlsFileNum { get; set; }
+
+        [StringLength(10)]
+        [Column(name: "callsign", Order = 3)]
+        public string? CallSign { get; set; }
+
+        [StringLength(10)]
+        [Column(name: "comment_date", Order = 4)]
+        public string? CommentDate { get; set; }
+
+        [StringLength(255)]
+        [Column(name: "description", Order = 5)]
+        public string? Description { get; set; }
+
+        [StringLength(1)]
+        [Column(name: "status_code", Order = 6)]
+        public string? StatusCode { get; set; }
+
+        [Column(name: "status_date", Order = 7)]
+        public DateTime? StatusDate { get; set; }
     }
 }
+
--- SamplePipeDelimiter/Program.cs
+++ SamplePipeDelimiter/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SamplePipeDelimiter;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        string line1 = "CO|1102336||KA42382||E||";
+        string line2 = "CO|1102461||KA49869||0||";
+        string line3 = "CO|1102685||KA66163||0RADAR SPEED MONITORING DEVIC||";
+    }
+}
